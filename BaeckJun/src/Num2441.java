8d790b770b7e45e6b45a969f5bfe657517b91705
jeongyeon-park <dus7653@gmail.com>
2020-08-21 15:21:23 +0900

    IO 별찍기-4

--- BaeckJun/src/Num2441.java
+++ BaeckJun/src/Num2441.java
@@ -3,15 +3,18 @@ import java.util.Scanner;
 public class Num2441 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		int total = scanner.nextInt();
+		int blank=0;
+		int star=total;
 		
-		int total=scanner.nextInt();
-		for(int i=0;i<total;i++) {
-			for(int j=total;j>0;j--) {
-				System.out.print('*');
-			}
-			System.out.print("\n");
-			total--;
+		for(int i=1; i<=total ; i++) {
+			for(int j=blank ; j<i-1 && i!=1; j++)
+				System.out.print(" ");
+			for(int k=star; k>0 ; k--)
+				System.out.print("*");
+			
+			star--;
+			System.out.println();
 		}
-		
 	}
 }
